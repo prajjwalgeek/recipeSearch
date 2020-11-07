ca17cbb3d9942f1ad3469d99832a46d85412f545
Prajjwal Mishra <43957680+prajjwalgeek@users.noreply.github.com>
2020-11-07 20:14:07 +0530

    Fixed the redux devtools error

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,9 +8,9 @@ const initialState = {}
 const middlewares = [thunk]
 
 const store = createStore(rootReducer, initialState, compose(
-    applyMiddleware(...middlewares))//,
-    // window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    // window.__REDUX_DEVTOOLS_EXTENSION__())
+    applyMiddleware(...middlewares)),
+    typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__())
 )
 
 
